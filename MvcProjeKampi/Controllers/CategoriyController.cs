a196a44facd1cb9c4622b3ca623f991cd719d68f
Samet Gürgen <sametgurgen@hotmail.com>
2021-05-02 02:08:47 +0300

    first commit

---
+++ MvcProjeKampi/Controllers/CategoriyController.cs
@@ -0,0 +1,36 @@
+﻿using BusinessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class CategoriyController : Controller
+    {
+        // GET: Categoriy
+        CategoryManager cm = new CategoryManager();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetCategoryList()
+        {
+            //var categoryvalues = cm.GetAllBL();
+            return View();
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+            //cm.CategoryAddBL(p);
+            return RedirectToAction("GetCategoryList");
+        }
+    }
+}
