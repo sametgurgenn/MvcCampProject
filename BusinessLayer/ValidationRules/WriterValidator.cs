ae2f134e8fdd7822c8297ea624b4ccfb57ad7d02
samet <samet@DESKTOP-LIGI7GR>
2021-05-15 05:36:57 +0300

    44ncü video son güncel

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator : AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Kategori Adını Boş Geçemezsiniz");
+            RuleFor(x => x.WriterSurname).NotEmpty().WithMessage("Açıklamayı Boş Geçemezsiniz");
+            RuleFor(x => x.WriterAbout).NotEmpty().WithMessage("Lütfen En Az 3 karakter girişi yapın");
+            RuleFor(x => x.WriterSurname).MinimumLength(2).WithMessage("Lütfen 20 karakterden fazla değer girişi yapmayın");
+        }
+    }
+}
